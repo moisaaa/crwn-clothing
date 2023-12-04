063296b0330382a30a33a5f3c7fc4fca87a3b462
360bpm <mrandrei32@gmail.com>
2023-12-05 01:27:03 +0200

    Made categories with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Directory from './components/directory/Directory';
+
+const App = () => {
+  return <Directory />;
+};
+
+export default App;
---
+++ src/components/category/Category.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Category = ({ category }) => {
+  const { id, title, imageUrl } = category;
+  return (
+    <div
+      key={id}
+      className="category border-4 flex items-center justify-center p-24 w-1/4 hover:scale-105 transition transform "
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="category-body border-2 p-4 flex flex-col justify-center items-center bg-white bg-opacity-75">
+        <h2>{title}</h2>
+
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
